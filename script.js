449babdd59138941e9a0dacaf21fb041972018ce
maryam-muneer <maryammuneer000@gmail.com>
2025-11-26 13:19:32 +0500

    2

--- script.js
+++ script.js
@@ -11,13 +11,22 @@ function greet() {
 // The function is stored in a variable. It does not get hoisted (you cannot use it before it appears)
 
 // practice questions
+// function declaration
 function addNumbers(a,b){
 console.log(a+b);
 }
 addNumbers(5,10);
 
+// function expression
 let fullName = function(firstName, lastName){
     return firstName + " " + lastName;
 }
 let personName = fullName("Maryam", "Muneer");
 console.log(personName);
+
+// arrow function
+let double=function(num){
+  return num*2;
+}
+double(5);
+console.log(double(5));S
